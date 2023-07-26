26d3bbc3e5d212920d21479dc55a18c5ebebc7fd
Bruno H. Homem <brunohhomem@gmail.com>
2023-07-26 09:19:46 -0300

    yesterday

--- src/curso/funcoes.java
+++ src/curso/funcoes.java
@@ -1,10 +1,39 @@
 package curso;
 
+import java.util.Scanner;
+
 public class funcoes {
     public static void main(String[] args) {
-        double y = 25.0;
-        double x = Math.sqrt(y);
 
-        System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira três números: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int maior = max(a, b, c);
+
+        showResult(maior);
+
+        sc.close();
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Maior = " + value);
     }
 }
